df0a3860b04e593f17de43080e52ec85153ce037
Gas Com IT <99112362+GasComIT@users.noreply.github.com>
2022-04-01 12:07:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + 'html/index.html'));
+  res.sendFile(path.join(__dirname + '/home.html'));
 });
 
 app.listen(3000, () => {
