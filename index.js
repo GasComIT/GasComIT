ef72c5e7c38d763ebeb6e92473a02b11082effd1
Gas Com IT <99112362+GasComIT@users.noreply.github.com>
2022-04-01 11:23:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + 'html/index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('server started successfully');
+});
